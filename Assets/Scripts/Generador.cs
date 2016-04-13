8bd1d21c603956f52c78d44364aa0ecf192e9f2c
brightside92 <alan_0192@hotmail.com>
2016-04-13 17:59:46 -0500

    regreso

--- Assets/Scripts/Generador.cs
+++ Assets/Scripts/Generador.cs
@@ -6,7 +6,6 @@ public class Generador : MonoBehaviour {
 	public GameObject[] obj;
 	public float tiempomin = 7f;
 	public float tiempomax = 8f;
-	bool grounded=false;
 	// Use this for initialization
 	void Start () {
 		generar ();
@@ -18,10 +17,7 @@ public class Generador : MonoBehaviour {
 	}
 
 	void generar(){
-		if (grounded && Input.touches.Length > 0) {
-		
-			Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
-			Invoke ("generar", Random.Range (tiempomin, tiempomax));
-		}
+		Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+		Invoke("generar", Random.Range(tiempomin,tiempomax));
 }
 }
---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+	public Renderer rend ;
+	public float velocidad =  0f;
+	void Start () {
+		rend = GetComponent <Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rend.material.mainTextureOffset = new Vector2 (Time.time * velocidad,0f);
+	}
+}
