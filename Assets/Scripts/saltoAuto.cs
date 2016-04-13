ea2dcb8f233a4f6ecb1ea2162cd45cd53f50d763
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2016-04-13 18:14:30 -0500

    Nivel2
    Cambios para el nivel2

---
+++ Assets/Scripts/saltoAuto.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class saltoAuto : MonoBehaviour {
+	float mov;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mov = Input.acceleration.x;
+		GetComponent<Rigidbody> ().AddForce (new Vector2(mov,0),ForceMode.Impulse);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		Debug.Log ("Choqué");
+		GetComponent<Rigidbody> ().AddForce (new Vector2(0,10),ForceMode.Impulse);
+	}
+}
