af60341dff7006bbb244681a5fad65f8c556a49d
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2016-04-12 23:51:58 -0500

    Código para que detecte la colisión con el español
    Ya está implementado el script. Falta hacerlo

---
+++ Assets/Scripts/collidesWithSpan.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collidesWithSpan : MonoBehaviour {
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.name.Equals("EspañolCuchillo(Clone)"))
+		{
+			Debug.Log ("Perdiste");
+		}
+	}
+}
