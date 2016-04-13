0a1b36dc0dc3f9de0a70764105183f5dc58cbfaf
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2016-04-12 19:49:39 -0500

    Caballo que no avanza rápido
    Ya no va rápido el caballo

--- Assets/Scripts/Avanza.cs
+++ Assets/Scripts/Avanza.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class Avanza : MonoBehaviour {
 	bool grounded=false;
+	float acl;
+	int cont;
 	// Use this for initialization
 	void Start () {
-	
+		cont = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (grounded ) {
-		GetComponent<Rigidbody> ().AddForce (new Vector2 (1,0), ForceMode.Impulse);
+		if (GetComponent<Rigidbody>().velocity.x< 4) {
+			//acl = (Time.deltaTime / Time.realtimeSinceStartup) + Time.fixedTime * Time.deltaTime;
+			acl=Time.realtimeSinceStartup*Time.deltaTime;
+			GetComponent<Rigidbody> ().AddForce (new Vector2 (acl, 0), ForceMode.Impulse);
 		}
 
+		cont++;
+		Debug.Log (acl);
+		Debug.Log ("Velocidad: "+GetComponent<Rigidbody>().velocity);
 }
 
 void OnCollisionEnter(Collision other){
