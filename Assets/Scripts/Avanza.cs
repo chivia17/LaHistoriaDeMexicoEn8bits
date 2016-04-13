0ca6629b815797b95c307ae96a2c1ba053745d22
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2016-04-12 23:50:56 -0500

    Quité código que no servía y aumenté un poco la velocidad
    Me desesperaba que fuera tan lento

--- Assets/Scripts/Avanza.cs
+++ Assets/Scripts/Avanza.cs
@@ -2,36 +2,21 @@
 using System.Collections;
 
 public class Avanza : MonoBehaviour {
-	bool grounded=false;
 	float acl;
-	int cont;
 	// Use this for initialization
 	void Start () {
-		cont = 1;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (GetComponent<Rigidbody>().velocity.x< 4) {
+	void Update () 
+	{
+		if (GetComponent<Rigidbody>().velocity.x< 6) {
 			//acl = (Time.deltaTime / Time.realtimeSinceStartup) + Time.fixedTime * Time.deltaTime;
 			acl=Time.realtimeSinceStartup*Time.deltaTime;
 			GetComponent<Rigidbody> ().AddForce (new Vector2 (acl, 0), ForceMode.Impulse);
 		}
 
-		cont++;
 		Debug.Log (acl);
 		Debug.Log ("Velocidad: "+GetComponent<Rigidbody>().velocity);
-}
-
-void OnCollisionEnter(Collision other){
-	if(other.gameObject.name.Equals("Plane")){
-		grounded = true;
 	}
-}
-
-void OnCollisionExit(Collision other){
-	if(other.gameObject.name.Equals("Plane")){
-		grounded = false;
-	}
-}
 }
