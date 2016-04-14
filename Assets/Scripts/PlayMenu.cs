1abc1b009c1741e44f9fc88aaab4e4304ceecab4
chivia17 <ziranda.chivas@gmail.com>
2016-04-13 22:53:38 -0500

    Script Menú
    Script del menú

---
+++ Assets/Scripts/PlayMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour {
+
+	void OnMouseDown(){
+
+		SceneManager.LoadScene(1);
+
+	}
+}
