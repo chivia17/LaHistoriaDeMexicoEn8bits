e3882d7f81bd6ebe8eadb10fc026db0c94caed2c
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2016-04-14 00:32:17 -0500

    nivel2
    más del nivel2

--- Assets/Scripts/saltoAuto.cs
+++ Assets/Scripts/saltoAuto.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class saltoAuto : MonoBehaviour {
 	float mov;
+	public static float altura;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		mov = Input.acceleration.x;
 		GetComponent<Rigidbody> ().AddForce (new Vector2(mov,0),ForceMode.Impulse);
+		altura = transform.position.y;
 	}
 
 	void OnCollisionEnter(Collision other)
